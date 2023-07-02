67870f748a1c1517f804ea20be5272ee2c449f29
Mohd Ikram <mdikram59319@gmail.com>
2023-07-02 23:52:52 +0530

    what is wrapper class

---
+++ wrapper class/src/wrapper/WrapperClass.java
@@ -0,0 +1,17 @@
+package wrapper;
+
+public class WrapperClass {
+	
+	public static void myClass(Integer myInt) {
+		System.out.println(myInt);
+	}
+	
+	public static void main(String[] args) {
+		
+		Integer myInteger=10000;
+		myClass(myInteger);
+	}
+	
+
+
+}
